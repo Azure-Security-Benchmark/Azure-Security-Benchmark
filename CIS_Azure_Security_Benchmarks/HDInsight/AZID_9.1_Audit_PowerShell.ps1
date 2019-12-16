2a93a6f8dc82626db2a122aa7b9395976d5f5114
Josh Madakor <josh.madakor@gmail.com>
2019-12-16 05:43:00 -0800

    HDI AZID 9.1 updated

--- CIS_Azure_Security_Benchmarks/HDInsight/AZID_9.1_Audit_PowerShell.ps1
+++ CIS_Azure_Security_Benchmarks/HDInsight/AZID_9.1_Audit_PowerShell.ps1
@@ -5,22 +5,18 @@ $HDIclusters = Get-AzHDInsightCluster
 if ($HDIclusters -ne $null) {
     foreach ($cluster in ($HDIclusters)) {
         $clusterObject = Get-AzResource -ResourceId $cluster.Id
-
         # Check if the current cluster is not associated with a Vnet
         if ($clusterObject.Properties.computeProfile.roles[0].virtualNetworkProfile -eq $null) {
-           
             # Clusters not associated with a Vnet will be displayed with a green background
             Write-Host "Cluster '$($cluster.Name)' is not secured within a Vnet." -BackgroundColor Red
         }
         else {
-            
             # Clusters associated with a Vnet will be displayed with a green background
             Write-Host "Cluster '$($cluster.Name)' has been secured within a Vnet." -BackgroundColor Green -ForegroundColor Black
         }
     }
 }
 else {
-    
     # If there are no HDInsight clusters within the current subscription, notify user
     Write-Host "No HDInsight clsuters exist on current subscription." -BackgroundColor Yellow -ForegroundColor Black
 }
